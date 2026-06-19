72c13d4d33f5934277f8ce32cf15f5c685b551dd
Huynh Tran Khanh Quynh <quynh190504@gmail.com>
2026-06-19 18:42:11 +0700

    Update LockdownAction.cs

--- Assets/Scripts/AI/Actions/Lockdown/LockdownAction.cs
+++ Assets/Scripts/AI/Actions/Lockdown/LockdownAction.cs
@@ -483,6 +483,9 @@ public class LockdownAction : HumanAction
 
         foreach (GridCell cell in cellList)
         {
+            if (neighbour.Stats.isLockdown)
+                continue;
+
             if (!disc.ContainsKey(cell))
             {
                 TarjanDFS(cell, null);
